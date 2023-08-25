103e1e7a927cdfa25562f83cde284e6b3b6304bc
Alice Rojas <alicemr273@gmail.com>
2023-08-25 00:35:03 -0600

    add Dashboard section tests

---
+++ src/components/dashboard/DashboardSearch.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-25 mw-50">
+    <b-input-group size="sm" class="mt-3 mb-4">
+      <b-input-group-prepend is-text>
+        <b-icon icon="search"></b-icon>
+      </b-input-group-prepend>
+      <b-form-input type="search" placeholder="Buscar"></b-form-input>
+    </b-input-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardSearch",
+};
+</script>
+
+<style></style>
---
+++ src/components/dashboard/DashboardTests.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box">
+    <div class="card card-tests">
+      <h3>Mis Evaluaciones</h3>
+      <DashboardSearch />
+      <DashboardTable />
+      <ModalDetailsTest />
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardSearch from "@/components/dashboard/DashboardSearch";
+import DashboardTable from "@/components/dashboard/DashboardTable";
+import ModalDetailsTest from "@/components/dashboard/ModalDetailsTest";
+
+export default {
+  name: "DashboardTests",
+  components: {
+    DashboardSearch,
+    DashboardTable,
+    ModalDetailsTest,
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  margin-top: 30px;
+  margin-left: 50px;
+  margin-right: 50px;
+  margin-bottom: 30px;
+}
+.card-tests {
+  border: 0;
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  align-items: flex-start;
+  padding: 30px;
+}
+</style>
